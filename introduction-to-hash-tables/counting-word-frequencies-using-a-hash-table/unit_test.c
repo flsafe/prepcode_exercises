28d56ff75c0c4264ee55538bdf12af931fbf2631
francisco licea <francisco.licea@gmail.com>
2011-07-15 01:09:41 -0500

    An external input file, fix a bug in insert function

--- introduction-to-hash-tables/counting-word-frequencies-using-a-hash-table/unit_test.c
+++ introduction-to-hash-tables/counting-word-frequencies-using-a-hash-table/unit_test.c
@@ -1,15 +1,48 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define MAX_WORD 32
+#define HASH_TAB 512
 
-int get_word(char *, int);
+/***
+* Do h1 and h2 need to be modulo HASH_TAB or is it enought that
+* hash is modulo HASH_TAB?
+*
+*
+***/
 
-void insert_word(char *w);
+struct wordrec {
+  char *word; 
+  int count;
+} wordtab[HASH_TAB];
 
-void count_word_freqs(void){
+unsigned int h1(char *word){
+  unsigned int hash;
+  char *c;
+  
+  c = word;
+  for(hash = 0 ; *c ; c++)
+    hash = hash * 37 + *c;
+  return hash;
+}
 
+unsigned int h2(char *word){
+  unsigned int hash;
+  char *c;
+  
+  c = word;
+  for(hash = 0 ; *c ; c++)
+    hash = hash * 31 + *c;
+  if (hash % 2 == 0)
+    return hash + 1;
+  else
+    return hash;
+}
 
+unsigned hash(char *word, int i){
+  return (h1(word) + i * h2(word)) % HASH_TAB;
 }
 
 int get_word(char *word, int limit){
@@ -23,7 +56,7 @@ int get_word(char *word, int limit){
     *w++ = c;
   } else {
     *w = '\0';
-    return word[0];
+    return c; 
   }
 
   for (; --limit > 0 ; w++)
@@ -35,12 +68,46 @@ int get_word(char *word, int limit){
   return word[0];
 }
 
-int main(){
+char *strdup(char *w){
+  char *d, *dest;
+
+  d = dest = malloc(strlen(w)+1);
+  while ((*d++ = *w++)) ;
+  return dest;
+}
+
+int insert_word(char *word){
+  int i;
+  int h;
+  
+  for (i = 0 ; i < HASH_TAB ; i++){
+    h = hash(word, i);
+    if (NULL == wordtab[h].word){
+      wordtab[h].word = strdup(word);
+      wordtab[h].count++;
+      return 1;
+    } else if (strcmp(wordtab[h].word, word) == 0){
+      wordtab[h].count++; 
+      return 1;
+    }
+  }
+  return 0;
+}
+
+void count_word_freqs(void){
+  int i;
   char word[MAX_WORD];
 
   while (get_word(word, MAX_WORD) != EOF){
-    printf("%s\n", word);
+    insert_word(word);
   }
 
+  for (i = 0 ; i < HASH_TAB ; i++)
+    if (NULL != wordtab[i].word)
+      printf("%s %d\n", wordtab[i].word, wordtab[i].count);
+}
+
+int main(){
+  count_word_freqs();
   return 0;
 }
