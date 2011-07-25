8510b69816fa1bb0dbdeb57728d6b16376d25d02
Francisco Licea <francisco.licea@gmail.com>
2011-07-25 18:07:26 -0500

    Make some functions shorter

--- introduction-to-hash-tables/hash_table.c
+++ introduction-to-hash-tables/hash_table.c
@@ -24,25 +24,24 @@ unsigned int h2(char *k){
 
   for (c = (unsigned char*)k ; *c ; c++)
     h = h * 31 + *c;
-  return h % 2 == 0 ? h+1 : h;
+  return h % 2 == 0 ? h + 1 : h;
 }
 
 unsigned int hash(char *k, int i){
   return (h1(k) + i * h2(k)) % HASH_TAB;
 }
 
-char *cpy(char *w){
-  char *d, *dest;
+char *cpy(char *s){
+  char *dest, *d;
 
-  d = dest = malloc(strlen(w) * sizeof(char) + 1);
-  if (d){
-    while ((*d++ = *w++)) ;
-  }
+  d = dest = malloc(strlen(s) * sizeof(char) + 1);
+  if (d)
+    while ((*d++ = *s++)) ; /* Copy */
   return dest;
 }
 
 int locate(struct hashrec hashtab[], char *k){
-  int i, b;
+  unsigned int i, b;
 
   for (i = 0 ; i < HASH_TAB ; i++){
     b = hash(k, i);
@@ -54,7 +53,7 @@ int locate(struct hashrec hashtab[], char *k){
 }
 
 int member(struct hashrec hashtab[], char *key){
-  int b = locate(hashtab, key);
+  unsigned int b = locate(hashtab, key);
   if (NULL == hashtab[b].key) 
     return 0;
   else
@@ -62,20 +61,19 @@ int member(struct hashrec hashtab[], char *key){
 }
 
 int insert(struct hashrec hashtab[], char *key){
-  int b;
+  unsigned int b;
 
   b = locate(hashtab, key);
   if (NULL == hashtab[b].key){
     hashtab[b].key = cpy(key);
     return 1;
-  } else if (strcmp(hashtab[b].key, key) == 0){
-    return 1;
-  } else
-    return 0;
+  }
+  else
+    return strcmp(hashtab[b].key, key) == 0;
 }
 
 void init_hash_table(struct hashrec hashtab[]){
-  int b;
+  unsigned int b;
 
   for (b = 0 ; b < HASH_TAB ; b++)
     hashtab[b].key = NULL;
